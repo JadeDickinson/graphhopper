3ccf00058703267d27fe5ed2bcd1dc666c5a889f
Jade Dickinson <jadedickinson@gmail.com>
2017-07-17 16:47:45 +0100

    Adjust imports and constructor

--- core/src/main/java/com/graphhopper/routing/weighting/KilocalorieWeighting.java
+++ core/src/main/java/com/graphhopper/routing/weighting/KilocalorieWeighting.java
@@ -2,10 +2,20 @@ package com.graphhopper.routing.weighting;
 
 import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.util.EdgeIteratorState;
+import com.graphhopper.util.PMap;
+import com.graphhopper.util.Parameters.Routing;
 
 public class KilocalorieWeighting extends FastestWeighting {
-    protected KilocalorieWeighting(FlagEncoder encoder) {
+    protected final static double SPEED_CONV = 3.6;
+    private final double headingPenalty;
+    private final long headingPenaltyMillis;
+    private final double maxSpeed;
+
+    public KilocalorieWeighting(FlagEncoder encoder, PMap map) {
         super(encoder);
+        headingPenalty = map.getDouble(Routing.HEADING_PENALTY, Routing.DEFAULT_HEADING_PENALTY);
+        headingPenaltyMillis = Math.round(headingPenalty * 1000);
+        maxSpeed = encoder.getMaxSpeed() / SPEED_CONV;
     }
 
     @Override
