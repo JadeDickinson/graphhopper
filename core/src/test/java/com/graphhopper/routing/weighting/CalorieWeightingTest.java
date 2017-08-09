cbc7b7451f7ed993facfad9e32f9a7a399a2005a
Jade Dickinson <jadedickinson@gmail.com>
2017-08-09 21:44:51 +0100

    Add failing test for basic calcMr() method

--- core/src/test/java/com/graphhopper/routing/weighting/CalorieWeightingTest.java
+++ core/src/test/java/com/graphhopper/routing/weighting/CalorieWeightingTest.java
@@ -103,4 +103,14 @@ public class CalorieWeightingTest {
         double time = instance.calcExactTimeInSeconds(virtualEdge);
         assertEquals(expectedTime, time, 0);
     }
+
+    @Test
+    public void testCalcMr() {
+        CalorieWeighting instance = new CalorieWeighting(encoder, new PMap());
+        VirtualEdgeIteratorState virtualEdge = new VirtualEdgeIteratorState(0, 1, 1, 2, 20,
+                encoder.setProperties(10, true, true), "test", Helper.createPointList3D(51.518224,-0.155107, 45, 51.518117,-0.155702, 38));
+        double expectedMR = 162.1804741456141;
+        double MR = instance.calcMr(virtualEdge);
+        assertEquals(expectedMR, MR, 0);
+    }
 }
