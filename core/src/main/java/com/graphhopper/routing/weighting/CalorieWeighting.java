2893473d990db730b30262c50f64c1a1b5553ade
Jade Dickinson <jadedickinson@gmail.com>
2017-08-09 19:13:11 +0100

    Implement calcElevationChange() - test passes

--- core/src/main/java/com/graphhopper/routing/weighting/CalorieWeighting.java
+++ core/src/main/java/com/graphhopper/routing/weighting/CalorieWeighting.java
@@ -40,7 +40,21 @@ public class CalorieWeighting extends AbstractWeighting {
     }
 
     public double calcElevationChange(EdgeIteratorState edge, boolean reverse) {
-        return 0;
+        PointList pl = edge.fetchWayGeometry(3);
+        Iterator itr = pl.iterator();
+        int i = 0;
+        double firstElevation = 0;
+        double secondElevation = 0;
+        while(itr.hasNext()) {
+            GHPoint3D element = (GHPoint3D)itr.next();
+            if(i == 0) {
+                firstElevation = element.getElevation();
+            } else {
+                secondElevation = element.getElevation();
+            }
+            i+=1;
+        }
+        return secondElevation - firstElevation;
     }
 
     @Override
